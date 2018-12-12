ece6c2aa3b6d595e6d1bb2fee218befd0c2a8f47
italo fontes <italo5123@hotmail.com>
2018-12-11 22:22:50 -0200

    refactored code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,80 +1,153 @@
+//change class
 
-function checkCashRegister(price, cash, cid){
-    const change = cash - price;
-    const valuesTbl = {
-        currency: ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'ONE', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'],
-        values: [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
+
+class Change{
+    constructor(price, cash, cid){
+        this.change = cash - price;
+        this.status = Change.calcStatus(this.change, cid);
+        this.transition = Change.makeTransition(this.change, cid, this.status);
+        
     }
 
-    const response = {
-        status: 'OPEN',
-        change: []
-    };
+    static calcStatus(change, cid){
 
-    let transitionMoney = 0;
-
-
-    
-
-    for(let i = cid.length -1; i >= 0; i--){
-        let currentCid = cid[i];
-        let currencyIndex = valuesTbl.currency.indexOf(currentCid[0]);
+        //sum all the cash-in-drawer
+        const totalCid = cid.map(currency => currency[1])
+                            .reduce( (a,b) => {
+                                return parseFloat((a + b).toFixed(2));
+                            });
         
-        let currentCurrency = valuesTbl.currency[currencyIndex],
-            currentValue = valuesTbl.values[currencyIndex];
+        
+        if(totalCid - change > 0) return 'OPEN';
+        else if(totalCid - change === 0) return 'CLOSED'
+        else return 'INSUFFICIENT_FUNDS';
 
-            const times = currentCid[1] / currentValue;
+    }
+
+    //the goal of this function is to make a transition taking the money from the cid, putting into a change variable and creating a new cid
+    //return a object with the old cid, new cid and change
+    //status parameter not required
+    static makeTransition(change, cid, status){
+
+        //values reference
+        const valuesTbl = {
+            currency: ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'ONE', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'],
+            values: [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
+        };
+
+        // will be composed
+        const returnObj = {
+            oldCid: cid,
+            newCid: [],
+            change: []
+        };
 
         
+        if(status === 'INSUFFICIENT_FUNDS'){
 
-        //compose change
-        for(let iTwo = 1; iTwo <= times; iTwo++){
-            console.log(currentCid[1], transitionMoney, currentValue, change);
-            
-            if(transitionMoney + currentValue <= change && currentCid[1] != 0){
-                currentCid[1] -= currentValue;
-                transitionMoney += currentValue;
+            returnObj.newCid = cid;
 
-                let cidItem = false;
-                
-                for(const [index, value] of response.change.entries()){
-                    if(value[0] === currentCurrency){
-                        cidItem = index;
-                        //console.log(value[0], currentCurrency, cidItem);
+        }else if(status === 'CLOSED'){
+
+            returnObj.newCid = cid.map(([currency, number]) => [currency, 0]);
+            returnObj.change = cid;
+
+
+        }else {
+
+            //the money will be taken from cid and placed on returnObj.change
+            //transitionMoney will be used to store the amount of money taken from the cid
+            let transitionMoney = 0;
+            let transitionCid = cid;
+
+            //loop through all cid itens(currecy and stored value of the currency)
+            for(let countOne = transitionCid.length -1; countOne >= 0; countOne--){
+
+                //cid item ex.: ["PENNY", 0.5]
+                let currentCid = transitionCid[countOne];
+
+                //index on valuesTbl.currency of the current currency
+                //ex.: if the current currency is 'PENNY' the returned index will be 0 
+                let currencyIndex = valuesTbl.currency.indexOf(currentCid[0]);
+
+                //get the value of the currency ex.: PENNY = 0.01
+                let currentCurrency = valuesTbl.currency[currencyIndex],
+                    currentValue = valuesTbl.values[currencyIndex];
+
+                //get how many units of the currency are in the cid
+
+                /* if the current cid is ["PENNY", 0.5], the 'times'
+                variable will be equal to 50, because there are 50 pennies
+                in the cid (0.5 * 0.01)
+                */
+                const times = Math.round(currentCid[1] / currentValue);
+
+                //loop until fulfill returnObj.change  with the change taken from transitionCid
+
+                for(let countTwo = 1; countTwo <= times; countTwo++){
+
+                    //checks if transitionMoney isn't fulfilled and equal to change
+                    let checkLimit = parseFloat((transitionMoney + currentValue).toFixed(2));
+
+                    if(checkLimit <= change && currentCid[1] != 0){
+
+                        //takes money from the cid and puts on transitionMoney and returnObj.change
+                        currentCid[1] -= currentValue;
+                        transitionMoney += currentValue;
+
+
+                        //checks if the returnObj.change already have a currency item, ex.: penny
+                        //then adds the value
+                        let cidItem = false;
+
+                        for(const [index, value] of returnObj.change.entries()){
+                            if(value[0] === currentCurrency){
+                                cidItem = index;
+                            }
+                        }
+
+                        if(typeof cidItem == 'number'){
+                            returnObj.change[cidItem][1] += currentValue;
+                        } else {
+                            returnObj.change.push([currentCurrency, currentValue]);
+                        }
                     }
                 }
-                if(typeof cidItem == 'number'){
-                    response.change[cidItem][1] += currentValue;
-                } else {
-                    response.change.push([currentCurrency, currentValue]);
-                }
-            }    
+            }
+
+            returnObj.newCid = transitionCid;
         }
         
-        
 
-        //console.log(currentCid, currentCurrency, currentValue, transitionMoney);
+        return returnObj;
+
 
     }
-    console.log(response);
-    console.log(cid);
 
+    fullChange(){
 
+        return {
+            status: this.status,
+            change: this.transition.change
+        };
 
-
+    }    
 }
 
 
 
+function checkCashRegister(price, cash, cid) {
+    const change = new Change(price, cash, cid);
+
+    return change.fullChange();
+  }
+
+
+
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
 
 
 
 
 
-
-
-
-
-
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
